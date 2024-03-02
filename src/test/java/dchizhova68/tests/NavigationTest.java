fd6309da4a02f4a56fdc718cebe6d3184cbce868
d.chizhova <dchizhova68@gmail.com>
2024-03-02 14:22:50 +0400

    bugFix

--- src/test/java/dchizhova68/tests/NavigationTest.java
+++ src/test/java/dchizhova68/tests/NavigationTest.java
@@ -6,6 +6,7 @@ import io.qameta.allure.Feature;
 import io.qameta.allure.Owner;
 import io.qameta.allure.Severity;
 import io.qameta.allure.SeverityLevel;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
@@ -34,6 +35,7 @@ public class NavigationTest extends TestBase {
     @DisplayName("Проверка видимости рекламного баннера")
     @Owner("Darya Chizhova")
     @Severity(SeverityLevel.NORMAL)
+    @Disabled("Баннер скрыт")
     void promoBannerVisibleTest() {
         mainPage.openPage()
                 .checkPromoBanner();
